4c544ca4ddd30c3b1f7627d44213fff162a34992
carita <motcheyomarcelle@yahoo.fr>
2022-10-25 01:57:27 -0400

    mywork

---
+++ ec2-instance.tf
@@ -0,0 +1,33 @@
+
+resource "aws_instance" "myfirst-instance" {
+  ami           = "ami-09d3b3274b6c5d4aa"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "focus"
+  }
+}
+
+resource "aws_instance" "mysecond-instance" {
+  ami           = "ami-09d3b3274b6c5d4aa"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "commitment"
+  }
+}
+
+
+resource "aws_instance" "mythirdinstance" {
+  ami           = "ami-09d3b3274b6c5d4aa"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "selfish"
+  }
+}
+
+
+
+
+
